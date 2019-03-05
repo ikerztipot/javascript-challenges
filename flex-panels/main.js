eb94944b9e21a435fc84f87b4aa0799b507af6cc
ikerztipot <iker93@gmail.com>
2019-03-05 19:46:09 +0100

    flex-panel added

---
+++ flex-panels/main.js
@@ -0,0 +1,20 @@
+
+window.onload = (e) => {
+
+    function toggleOpen(){
+        this.classList.toggle('open');
+    }
+    
+    function toggleActive(e) {
+        console.log(e.propertyName);
+        if( e.propertyName.includes('flex')) {
+            this.classList.toggle('open-active');
+        }
+    }
+
+    const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+};
